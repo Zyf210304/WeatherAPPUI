77682b00aa436cdff883b0f6afc2e8464fe39988
张亚飞 <672233580@qq.com>
2021-06-17 13:32:32 +0800

    Initial Commit

---
+++ WeatherAPPUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherAPPUI
+//
+//  Created by 张亚飞 on 2021/6/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherAPPUI/WeatherAPPUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPPUIApp.swift
+//  WeatherAPPUI
+//
+//  Created by 张亚飞 on 2021/6/17.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPPUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
